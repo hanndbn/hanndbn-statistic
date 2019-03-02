1f93bd26eb2dcf37e81f308351251b4ff6af5f8d
BlueSky <han.nguyendinh45@gmail.com>
2019-03-03 01:51:46 +0700

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/hannd/statistic/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.hannd.statistic.security;
---
+++ src/main/java/com/hannd/statistic/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package com.hannd.statistic.web.websocket.dto;
